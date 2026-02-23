20b1192e5bae3d4f032b06a4845133841b858071
Pereira <126605420+WPereiraJ@users.noreply.github.com>
2026-02-22 22:22:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const clientId = process.env.CLIENT_ID;
 const guildId = process.env.GUILD_ID;
 
 // === COLOQUE O ID DO SEU CARGO FIXO AQUI ===
-const CARGO_JOGADORES_ID = '1475291993084137532';
+const CARGO_JOGADORES_ID = '1475300658923045128';
 
 const client = new Client({ 
     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.DirectMessages],
@@ -417,4 +417,5 @@ const express = require('express');
 const app = express();
 app.get('/', (req, res) => res.send('O Bot do Mural de RPG está online e operante!'));
 const port = process.env.PORT || 3000;
+
 app.listen(port, () => console.log(`Servidor web de mentirinha rodando na porta ${port}`));
