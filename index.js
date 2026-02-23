e0ab4e34aa09fecd9adc74a186da7edf074a7f8e
Pereira <126605420+WPereiraJ@users.noreply.github.com>
2026-02-22 22:45:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const clientId = process.env.CLIENT_ID;
 const guildId = process.env.GUILD_ID;
 
 // === COLOQUE O ID DO SEU CARGO FIXO AQUI ===
-const CARGO_JOGADORES_ID = 'COLE_O_ID_AQUI';
+const CARGO_JOGADORES_ID = '1475291993084137532';
 
 const client = new Client({ 
     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.DirectMessages],
@@ -135,7 +135,7 @@ const commands = [
         .setDescription('Abre o formulário para criar uma missão no mural.')
         .addIntegerOption(opt => opt.setName('nd').setDescription('Nível de Desafio Base').setRequired(true).addChoices(...ndChoices))
         .addStringOption(opt => opt.setName('dificuldade').setDescription('Dificuldade').setRequired(true)
-            .addChoices({ name: 'Normal', value: 'normal' }, { name: 'Difícil', value: 'dificil' }, { name: 'Tormenta 20%', value: 'tormenta' }))
+            .addChoices({ name: 'Normal', value: 'normal' }, { name: 'Difícil', value: 'dificil' }, { name: 'Tormenta', value: 'tormenta' }))
         .addStringOption(opt => opt.setName('data_hora').setDescription('Data e horário da sessão').setRequired(true))
 ].map(command => command.toJSON());
 
@@ -411,3 +411,4 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Servidor web de mentirinha rodando na porta ${port}`));
 
 client.login(token);
+
