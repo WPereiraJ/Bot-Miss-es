a86a6d1088ef733d7e82d9aff51d04c02676a5c3
Pereira <126605420+WPereiraJ@users.noreply.github.com>
2026-02-22 22:36:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -134,7 +134,7 @@ const commands = [
         .setDescription('Abre o formulário para criar uma missão no mural.')
         .addIntegerOption(opt => opt.setName('nd').setDescription('Nível de Desafio').setRequired(true).addChoices(...ndChoices))
         .addStringOption(opt => opt.setName('dificuldade').setDescription('Dificuldade').setRequired(true)
-            .addChoices({ name: 'Normal', value: 'normal' }, { name: 'Difícil', value: 'dificil' }, { name: 'Tormenta 20%', value: 'tormenta' }))
+            .addChoices({ name: 'Normal', value: 'normal' }, { name: 'Difícil', value: 'dificil' }, { name: 'Tormenta', value: 'tormenta' }))
         .addStringOption(opt => opt.setName('data_hora').setDescription('Data e horário da sessão').setRequired(true))
 ].map(command => command.toJSON());
 
@@ -419,3 +419,4 @@ app.get('/', (req, res) => res.send('O Bot do Mural de RPG está online e operan
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Servidor web de mentirinha rodando na porta ${port}`));
+
